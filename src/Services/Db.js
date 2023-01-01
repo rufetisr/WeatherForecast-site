ae8cdd26361e1d1db32748d6b5651bffe3c38af5
rufetisr <rufet.isr123@gmail.com>
2023-01-01 06:14:20 -0800

    upd

--- src/Services/Db.js
+++ src/Services/Db.js
@@ -7,7 +7,12 @@ export const getDataFromApi = async (city) => {
     // let {setDaily, daily } = useContext(Context)
 
     const key = '2f9270987154c3c51798342a0a80f322';
-    let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${key}`);
+    let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${key}`).catch(
+        (error) => {
+           alert('This city was not found!');
+           return Promise.reject(error);
+       }
+   );;
 
     return res;
 }
