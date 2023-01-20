10c5a306b3997f32da8b4a105e695be3f63c9fc2
rufetisr <rufet.isr123@gmail.com>
2023-01-20 01:43:37 -0800

    updated

--- src/Pages/Feedback.jsx
+++ src/Pages/Feedback.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import './Feedback.css'
 import context from '../Context/Context';
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import * as CiIcons from 'react-icons/ci'
 
 const Feedback = () => {
   const { color } = useContext(context)
   const ul = useRef()
+
+  const resetColor = () => {
+    ul.current.children[0].firstChild.style.color = 'black'
+    ul.current.children[1].firstChild.style.color = 'black'
+    ul.current.children[2].firstChild.style.color = 'black'
+  }
+
   const choiceSmile = (e) => {
+    resetColor();
     if (ul.current.children[0].firstChild == e.target) {
-      ul.current.children[1].firstChild.style.color = 'black'
-      ul.current.children[2].firstChild.style.color = 'black'
+      // ul.current.children[1].firstChild.style.color = 'black'
+      // ul.current.children[2].firstChild.style.color = 'black'
 
       e.target.style.color = 'rgb(214, 47, 6)';
     }
     else if (ul.current.children[1].firstChild == e.target) {
-      ul.current.children[0].firstChild.style.color = 'black'
-      ul.current.children[2].firstChild.style.color = 'black'
+      // ul.current.children[0].firstChild.style.color = 'black'
+      // ul.current.children[2].firstChild.style.color = 'black'
       e.target.style.color = 'rgb(196, 153, 14)';
     }
-    else if (ul.current.children[2].firstChild == e.target) {
-      ul.current.children[0].firstChild.style.color = 'black'
-      ul.current.children[1].firstChild.style.color = 'black'
 
+    else if (ul.current.children[2].firstChild == e.target) {
+      // ul.current.children[0].firstChild.style.color = 'black'
+      // ul.current.children[1].firstChild.style.color = 'black'
       e.target.style.color = 'rgb(113, 168, 11)'
+
     }
+
   }
 
-  const SubmitData = (e)=>{
+  const SubmitData = (e) => {
     e.preventDefault()
   }
-  
+
   return (
     <div className={`feed ${color}`}>
       <h1>Your Feedback</h1>
@@ -39,9 +49,9 @@ const Feedback = () => {
         <p>We would like your feedback to improve our website.</p>
         <p>What is your opinion abot this site?</p>
         <ul className='smile' ref={ul}>
-          <li onClick={choiceSmile}><CiIcons.CiFaceFrown /></li>
-          <li onClick={choiceSmile}><CiIcons.CiFaceMeh /></li>
-          <li onClick={choiceSmile}><CiIcons.CiFaceSmile /></li>
+          <li onClick={choiceSmile} ><CiIcons.CiFaceFrown /></li>
+          <li onClick={choiceSmile} ><CiIcons.CiFaceMeh /></li>
+          <li onClick={choiceSmile} ><CiIcons.CiFaceSmile /></li>
         </ul>
         <hr style={{ maxWidth: '400px', width: '100%' }} />
         <form onSubmit={SubmitData}>
