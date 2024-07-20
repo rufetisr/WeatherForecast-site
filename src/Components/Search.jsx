c12f07a19582eeea91620931e5238961f69e598a
rufetisr <rufet.isr123@gmail.com>
2024-07-20 14:23:52 +0400

    cleared sensitivie informations from the code

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -25,26 +25,13 @@ const Search = () => {
         setDate(result.data.dt);
         // console.log(lat, lon);
         setImg(`http://openweathermap.org/img/wn/${result.data.weather[0].icon}@2x.png`)
-        console.log(result);
 
-        // const key = '2f9270987154c3c51798342a0a80f322';
-        // let city = e.target.city.value;
+
         fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`).then(res => res.json()).then(data => {
             console.log(data);
             setDaily(data);
         });
-        // e.target.city = null;
-        // const key1 = '2f9270987154c3c51798342a0a80f322';
-        // fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${result.data.coord.lat}&lon=${result.data.coord.lon}&appid=${key1}`)
-        // .then(res=>res.json())
-        // .then(data=>{
-        //     console.log(data);
-        //     setHourly(data); 
-        // });
-        // let res = fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&units=metric&appid=${key1}`).then(res=>res.json()).then(data=>{
-        //     setHourly(data);
-        //     console.log(data);
-        // });
+
     }
     let clickSave = (e) => {
         e.preventDefault();
--- src/Services/Db.js
+++ src/Services/Db.js
@@ -3,12 +3,9 @@ import axios from "axios";
 // import apiKey from './Apikey';
 
 export const getDataFromApi = async (city) => {
-    // let {setDaily, daily,  } = useContext(Context)
 
-    // const key = '2f9270987154c3c51798342a0a80f322';
     const apiKey = import.meta.env.VITE_API_KEY
 
-    console.log(apiKey);
 
 
     let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).catch(
